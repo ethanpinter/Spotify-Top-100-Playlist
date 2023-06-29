3072ca7b529ccba7abe2de71c3c141551ab8dab2
ethanpinter <ethanpinter@psu.edu>
2023-06-29 00:56:25 -0400

    rm auth for lastfm (unneeded) added get_top_tracks

--- lastfmAPI.py
+++ lastfmAPI.py
@@ -7,10 +7,12 @@ Library script for interacting with Last.fm data
 '''
 
 # imports
+from hashlib import md5
 import requests
 from secret import secret
 import json
 import webbrowser
+from urllib.parse import urlencode
 
 # globals
 LASTFM_API_KEY = secret.get('LASTFM_API_KEY')
@@ -18,36 +20,7 @@ LASTFM_SECRET = secret.get('LASTFM_SECRET')
 BASE_URL = 'http://ws.audioscrobbler.com/2.0/'
 
 class lastfmAPI:
-    def get_token():
-        '''
-        # Retrieve an access token for Last.fm
-        :return: string - access token
-        '''
-        endpoint = BASE_URL + f'?method=auth.gettoken&api_key={LASTFM_API_KEY}&format=json'
-        resp = requests.post(endpoint)
-        data = json.loads(resp.text)
-        return data['token']
-    
-    def auth_token(token):
-        '''
-        # Authenticate an access token using the browser
-        :return: None
-        '''
-        chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
-        url = f'http://www.last.fm/api/auth/?api_key={LASTFM_API_KEY}&token={token}'
-        webbrowser.get(chrome_path).open(url)
-
-    ## needs fixed
-    def get_session(token):
-        '''
-        # Retrieve an session token for Last.fm
-        :return: string - session token
-        '''
-        params = {
-            'api-key': LASTFM_API_KEY,
-            'method': 'auth.getsession',
-            'format': 'json'
-        }
-        resp = requests.get(BASE_URL, params=params)
-        #data = json.loads(resp.text)
-        return resp
+    def get_top_tracks(user, limit = 100):
+        url = BASE_URL + f'?method=user.gettoptracks&user={user}&api_key={LASTFM_API_KEY}&format=json&limit={limit}&period=7day'
+        resp = requests.get(url)
+        return json.loads(resp.content)
