00c949f285a887121fe8153fec2e1869aa9626e7
ethanpinter <ethanpinter@psu.edu>
2023-07-16 23:30:03 -0400

    added main launcher

---
+++ LastifyTop100.py
@@ -0,0 +1,30 @@
+'''
+LastifyTop100.py
+
+# Script for creating my Top 100 Recent songs on Spotify
+# based on data from Last.FM
+
+@ethanpinter
+'''
+
+# imports
+import spotifyAPI as sa
+import lastfmAPI as last
+
+def main():   
+    ## auth
+    auth_code = sa.spotifyAPI.request_user_auth()
+    access_token = sa.spotifyAPI.get_access_token(auth_code)
+    ## get tracks from last
+    tracksArtists = last.lastfmAPI.get_top_tracks_and_artists('ethanpinter')
+    ## get the spotify ids for songs
+    tracksIDs = sa.spotifyAPI.get_track_id_by_name(tracksArtists, access_token)
+    ## create a playlist and add songs to it
+    playlist_id = sa.spotifyAPI.create_playlist(access_token)
+    for track in tracksIDs:
+        sa.spotifyAPI.add_track_to_playlist(track, playlist_id, access_token)
+    ## modify playlist cover
+    ## TO-DO
+
+if __name__ == '__main__':
+    main()
